e97e7666f03152a1169dfde5069d2def1e264fba
Chandni Hablani <chandnihablani2@gmail.com>
2025-12-26 00:09:04 +0500

    reverse alphabet
    Print z to a characters

---
+++ Tasks/loop.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	
+	cout<<"print the a to z "<<endl;
+	
+	
+	
+	for(char a= 'a'; a<='z'; a++)
+	{
+		cout<<a;
+		cout<< endl;
+		
+	}
+	cout<<"print the reverse order ";
+	cout<<endl;
+	
+	
+	for(char b='z';b>='a'; b--)
+	{
+		cout<<b;
+		cout<<endl;
+	}
+	
+	
+	
+	
+	
+	
+}

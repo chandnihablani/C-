8089ba05e2d507565a36affaa12a790c4b32ac1c
Chandni Hablani <chandnihablani2@gmail.com>
2025-12-28 11:02:34 +0500

    Add files via upload
    This C++ program checks the strength of a password entered by the user.
    It scans each character of the password and checks whether it contains at least one uppercase letter, one lowercase letter, one digit, and one special character.
    If all these conditions are satisfied, the program prints “Strong Password”; otherwise, it prints “Weak Password”.
    The program uses character arrays, loops, and conditional statements to perform the validation.

---
+++ Tasks/PasswordStrengthChecker.cpp
@@ -0,0 +1,29 @@
+// Tough Program: Password Strength Checker
+#include <iostream>
+using namespace std;
+
+int main() {
+    char pass[50];
+    int i = 0, upper = 0, lower = 0, digit = 0, special = 0;
+
+    cout << "Enter password: ";
+    cin >> pass;
+
+    while(pass[i] != '\0') {
+        if(pass[i] >= 'A' && pass[i] <= 'Z')
+            upper = 1;
+        else if(pass[i] >= 'a' && pass[i] <= 'z')
+            lower = 1;
+        else if(pass[i] >= '0' && pass[i] <= '9')
+            digit = 1;
+        else
+            special = 1;
+        i++;
+    }
+
+    if(upper && lower && digit && special)
+        cout << "Strong Password";
+    else
+        cout << "Weak Password";
+}
+
